cd72437babbd527c48d49a2d14334a8ce4fc1799
Nino <anthony@delcey.fr>
2021-06-11 19:16:18 +0200

    Add callback creation for Room

--- app/src/main/java/com/example/todoc/ViewModelFactory.java
+++ app/src/main/java/com/example/todoc/ViewModelFactory.java
@@ -5,10 +5,13 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.todoc.data.database.AppDatabase;
 import com.example.todoc.repository.TaskRepository;
 import com.example.todoc.task.TasksViewModel;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -30,7 +33,21 @@ public class ViewModelFactory implements ViewModelProvider.Factory {
             synchronized (ViewModelFactory.class) {
                 if (factory == null) {
 
-                    RoomDatabase.Builder<AppDatabase> builder = Room.databaseBuilder(MainApplication.getApplication(), AppDatabase.class, "app_database");
+                    RoomDatabase.Builder<AppDatabase> builder = Room.databaseBuilder(
+                        MainApplication.getApplication(),
+                        AppDatabase.class,
+                        "app_database"
+                    ).addCallback(new RoomDatabase.Callback() {
+                        @Override
+                        public void onCreate(@NonNull @NotNull SupportSQLiteDatabase db) {
+                            super.onCreate(db);
+
+                            // TODO FADEL Insert initial projects
+                            db.beginTransaction();
+
+                            // db.execSQL();
+                        }
+                    });
                     if (BuildConfig.DEBUG) {
                         builder.fallbackToDestructiveMigration();
                     }
